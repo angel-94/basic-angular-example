9bcd419315609e0be107180ad077a39d5312e2ac
Manuel Angel <angel138ms@gmail.com>
2018-06-29 22:13:57 -0500

    initial commit with all files generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {BlankLayoutComponent} from './blank-layout/blank-layout.component';
+import {AppLayoutComponent} from './app-layout/app-layout.component';
+import {CustomMaterialModule} from "./custom-material/custom-material.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlankLayoutComponent,
+    AppLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
