e26fccbf21ef426c1361a7fb947ac9587063d458
Angel Ruiz <angel138ms@gmail.com>
2019-03-19 12:51:13 -0600

    Add speech Recognition

--- src/app/app-components/home/home.component.ts
+++ src/app/app-components/home/home.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SpeechRecognitionService} from "@kamiazya/ngx-speech-recognition";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    providers: [
+        SpeechRecognitionService,
+    ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    public started = false;
+    public message = '';
 
-  ngOnInit() {
-  }
+    frmSpeechDemo: FormGroup;
+    finalMessage: any;
+
+    constructor(private speechService: SpeechRecognitionService,) {
+    }
+
+    ngOnInit() {
+
+        this.initFrmSpeechDemo();
+        console.log('HomeComponent --> ', this.speechService);
+        this.speechService.onstart = (e) => {
+            console.log('onstart', e);
+        };
+        this.speechService.onresult = (e) => {
+            this.message = e.results[0].item(0).transcript;
+            console.log('HomeComponent:onresult ---> ', this.message, e);
+        };
+
+    }
+
+    start() {
+        this.started = true;
+        this.speechService.start();
+    }
+
+    stop() {
+        this.started = false;
+        this.speechService.stop();
+    }
+
+    initFrmSpeechDemo() {
+        this.frmSpeechDemo = new FormGroup({
+            speech: new FormControl()
+        })
+    }
+
+    onSubmit() {
+        console.log('form value --> ', this.frmSpeechDemo.value);
+        this.finalMessage = this.frmSpeechDemo.value;
+        this.frmSpeechDemo.reset();
+    }
+
+    valueForSpeechField() {
+        this.speechService.start();
+        this.speechService.onresult = (e) => {
+            let message = e.results[0].item(0).transcript;
+            this.frmSpeechDemo.controls['speech'].setValue(message);
+            if (e.results[0].isFinal) {
+                this.speechService.stop();
+                alert('Speech Recognition is ending. Please try again!');
+            }
+            console.log('HomeComponent:onresult ---> ', message, e);
+        };
+    }
 
 }
